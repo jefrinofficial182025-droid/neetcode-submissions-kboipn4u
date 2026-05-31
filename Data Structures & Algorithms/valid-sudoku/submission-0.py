21564d2a112ba37a567dbb195ad16e6191e5571a
jefrinofficial182025-droid <jefrinofficial182025@gmail.com>
2026-05-31 13:38:31 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for r in range(9):
+            visited=set()
+            for c in range(9): 
+                if board[r][c]=='.':
+                    continue
+                if board[r][c] in visited:
+                    return False
+                visited.add(board[r][c])
+        for c in range(9):
+            visited=set()
+            for r in range(9): 
+                if board[r][c]=='.':
+                    continue
+                if board[r][c] in visited:
+                    return False
+                visited.add(board[r][c])
+        for r in range(0,9,3):
+            for c in range(0,9,3): 
+                visited=set()
+                for rs in range(r,r+3):
+                    for cs in range(c,c+3): 
+                        if board[rs][cs]=='.':
+                            continue
+                        if board[rs][cs] in visited: 
+                            return False
+                        visited.add(board[rs][cs])
+
+        return True
